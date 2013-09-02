f4c2b28eff0a480823efe75180e8cd381113a16e
Sergey Solovyev <se.solovyev@gmail.com>
2013-09-02 23:56:51 +0400

    Lawnmower

---
+++ src/main/java/org/solovyev/problems/googlecodejam/q2013/Lawnmower.java
@@ -0,0 +1,20 @@
+package org.solovyev.problems.googlecodejam.q2013;
+
+import org.solovyev.problems.googlecodejam.GoogleCodeJamProblem;
+
+import javax.annotation.Nonnull;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+/**
+ * User: serso
+ * Date: 9/2/13
+ * Time: 11:46 PM
+ */
+public class Lawnmower extends GoogleCodeJamProblem {
+	@Nonnull
+	@Override
+	protected CharSequence solve(@Nonnull String line, @Nonnull BufferedReader sr) throws IOException {
+		return null;
+	}
+}
