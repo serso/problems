f9bc183344412a77e3a07e96d4646445d2c95252
Sergey Solovyev <se.solovyev@gmail.com>
2013-09-03 21:31:33 +0400

    fixes

--- src/main/python/org/solovyev/problems/SmsCount.py
+++ src/main/python/org/solovyev/problems/SmsCount.py
@@ -1,6 +1,22 @@
 # http://habrahabr.ru/post/191498/
 
 
+def countStrings(k):
+    m = [[0 for x in range(k)] for x in range(k)]
+
+    # i - length of the string
+    # j - number of presses
+    for i in range(0, k):
+        for j in range(0, k):
+            oneSymbolCount = getCount(m, i - 1, j - 1)
+            twoSymbolsCount = getCount(m, i - 1, j - 2)
+            threeSymbolsCount = getCount(m, i - 1, j - 3)
+            fourSymbolsCount = getCount(m, i - 1, j - 4)
+            m[i][j] = (oneSymbolCount + twoSymbolsCount + threeSymbolsCount) * 8 + fourSymbolsCount * 2
+
+    return sum(m[i][j] for i in range(0, k) for j in range(0, k))
+
+
 def getCount(m, i, j):
     if i == -1 and j == -1:
         return 1
@@ -10,17 +26,7 @@ def getCount(m, i, j):
 
     return m[i][j]
 
+
 k = int(input("Enter k: \n"))
-m = [[0 for x in range(k)] for x in range(k)]
 
-# i - length of the string
-# j - number of presses
-for i in range(0, k):
-    for j in range(0, k):
-        oneSymbolCount = getCount(m, i - 1, j - 1)
-        twoSymbolsCount = getCount(m, i - 1, j - 2)
-        threeSymbolsCount = getCount(m, i - 1, j - 3)
-        fourSymbolsCount = getCount(m, i - 1, j - 4)
-        m[i][j] = (oneSymbolCount + twoSymbolsCount + threeSymbolsCount) * 8 + fourSymbolsCount * 2
-
-print("Total: " + repr(sum(m[i][j] for i in range(0, k) for j in range(0, k))))
+print("Total: " + repr(countStrings(k)))
