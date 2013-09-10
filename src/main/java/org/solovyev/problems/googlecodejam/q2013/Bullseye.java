a5064997a547ff71ad3866a1919642068d3b798d
Sergey Solovyev <se.solovyev@gmail.com>
2013-09-11 00:27:35 +0400

    bullseye

---
+++ src/main/java/org/solovyev/problems/googlecodejam/q2013/Bullseye.java
@@ -0,0 +1,73 @@
+package org.solovyev.problems.googlecodejam.q2013;
+
+import org.solovyev.problems.googlecodejam.GoogleCodeJamProblem;
+
+import javax.annotation.Nonnull;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.math.BigInteger;
+
+import static java.math.BigInteger.ONE;
+import static java.math.BigInteger.ZERO;
+
+// http://code.google.com/codejam/contest/2418487/dashboard#s=p0&a=0
+public class Bullseye extends GoogleCodeJamProblem{
+
+	public static final BigInteger TWO = BigInteger.valueOf(2l);
+	public static final BigInteger THREE= BigInteger.valueOf(3l);
+	public static final BigInteger FOUR = BigInteger.valueOf(4l);
+
+	@Nonnull
+	@Override
+	protected CharSequence solve(@Nonnull String line, @Nonnull BufferedReader br) throws IOException {
+		final String[] arguments = line.split(" ");
+
+		final BigInteger r = new BigInteger(arguments[0]);
+		final BigInteger paint = new BigInteger(arguments[1]);
+		return solve(r, paint).toString();
+	}
+
+	@Nonnull
+	BigInteger solve(@Nonnull BigInteger r, @Nonnull BigInteger paint) {
+		return findK(ONE, k(r, paint), r, paint);
+	}
+
+	private BigInteger findK(@Nonnull BigInteger left, @Nonnull BigInteger right, @Nonnull BigInteger r, @Nonnull BigInteger paint) {
+		final BigInteger center = right.subtract(left).divide(TWO);
+		if(center.compareTo(ZERO) == 0) {
+			return ONE;
+		}
+
+		if(center.compareTo(left) < 0) {
+			return left;
+		}
+
+		if(center.compareTo(right) > 0) {
+			return right;
+		}
+		final BigInteger paintKRings = paintKRings(r, center);
+		if(paintKRings.compareTo(paint) < 0) {
+			return findK(center, right, r, paint);
+		} else if (paintKRings.compareTo(paint) > 0) {
+			return findK(left, center, r, paint);
+		} else {
+			return center;
+		}
+	}
+
+	private static final BigInteger paintKRings(final BigInteger r, final BigInteger k) {
+		return k.multiply(s0(r).add(TWO.multiply(k.add(ONE))));
+	}
+
+	private static final BigInteger s0(final BigInteger r) {
+		return TWO.multiply(r).add(ONE);
+	}
+
+	private static final BigInteger paintKRing(final BigInteger r, final BigInteger k) {
+		return TWO.multiply(r).add(FOUR.multiply(k)).subtract(THREE);
+	}
+
+	private static final BigInteger k(final BigInteger r, final BigInteger s) {
+		return s.subtract(TWO.multiply(r)).add(THREE).divide(FOUR);
+	}
+}
